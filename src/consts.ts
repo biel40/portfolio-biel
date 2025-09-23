a40d919a7c56f810b8deae2e13e55db656b64bb3
biel40 <bielet40@gmail.com>
2025-09-23 14:42:40 +0200

    fixed experience view web portfolio

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Portfolio de Biel';
-export const SITE_DESCRIPTION = 'Bienvenido a mi portafolio web digital. Aquí encontrarás información sobre mi experiencia, proyectos y habilidades. ¡Espero que te guste!';
+export const SITE_DESCRIPTION = '¡Bienvenido a mi portafolio web digital! Aquí encontrarás información sobre mi experiencia, proyectos y habilidades. ¡Espero que te guste!';
